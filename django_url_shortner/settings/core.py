42616036e6666f84c5c5094c6dac631f48361851
hossain-shuvo7860 <shahadat@baiust.edu.bd>
2021-06-12 21:57:17 +0600

    aa

--- django_url_shortner/settings/core.py
+++ django_url_shortner/settings/core.py
@@ -13,7 +13,7 @@ import os.path
 from pathlib import Path
 import django_heroku
 import dj_database_url
-
+#abc
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
